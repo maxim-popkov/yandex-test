dfc0e9d9a3a091da9b9997859129e3ab36eb8132
maxim-popkov <maxim.popkov@outlook.com>
2013-08-19 01:36:34 +0400

    Modified Status

--- scripts.js
+++ scripts.js
@@ -30,16 +30,46 @@
 		Inputer.prototype.nextField = function () {
 
 		};
+		Inputer.prototype.isFinished = function () {
+			var i = this.qst.length;
+			while(--i >= 0){
+				if (this.qst[i] === false) {
+					return false;
+				}
+			}
+			return true;
+		};
+		Inputer.prototype.ShowConfirmSubmit = function () {
+			$(".main-fastInput__taWrap").hide();
+			var outLbl = document.getElementById("main-fastInput_lbl");
+			this.sendBtn.innerText = "в Яндекс!";
+			outLbl.innerText = "Отправить данные в Яндекс"; 
+
+		};
+
 		Inputer.prototype.isFieldAnswered = function () {
 
 		};
 		Inputer.prototype.nextField = function () {
+			var nextFieldNum = -1;
+			var i = 0;
+			var isFind = false;
+			while(this.qst[i] == true && i < this.qst.length){
+				i++;
+			}
+			console.log(this.qst[i], "   ", i);
+			if (this.qst[i] === false) {
+				isFind = true;
+				nextFieldNum = i;
+				this.curr_field = i;
+			}
+			return nextFieldNum;
 
 		};
 		Inputer.prototype.submit = function () {
 			var txtArea = document.getElementById('main-fastInput--ta');
-			var outElem = $(".main__form_table textarea:eq(" + this.curr_field + " )");
-
+			var outElem = $(".main-form__qstion--ta:eq(" + this.curr_field + " )");
+			console.log(txtArea.value);
 			//todo verify
 			this.qst[this.curr_field] = true;
 			outElem.val(txtArea.value);
@@ -55,20 +85,15 @@
 		};
 		Inputer.prototype.showNextQst = function(){
 			//todo cycle next
-			var nextFieldNum = -1;
-			var i = 0;
-			var isFind = false;
-			while(this.qst[i] == true && i < this.qst.length){
-				i++;
-			}
-			if (this.qst[i] === false) {
-				isFind = true;
-				nextFieldNum = i;
-				this.curr_field = i;
-			};
-			var outElem = document.getElementById('main-fastInput_lbl');
-			outElem.innerText = $(".main__form_table label:eq(" + nextFieldNum + " )").text();
+			var nextFieldNum = this.nextField();
+			if (nextFieldNum > -1) {
+				var outElem = document.getElementById('main-fastInput_lbl');
+				outElem.innerText = $(".main-form__qstion--lbl:eq(" + nextFieldNum + " )").text();
 
+			}else{
+				this.ShowConfirmSubmit();
+			}
+			
 		}
 		return Inputer;
 	}());
@@ -77,7 +102,7 @@
 	window.onload = function init(){
 		var inputerDiv = document.getElementById('Inputer');
 		var sendBtn = document.getElementById('main-fastInput--sendBtn');
-		var inputer = new Inputer(inputerDiv, sendBtn,"abra",0,0,15);
+		var inputer = new Inputer(inputerDiv, sendBtn,"abra",0,0,3);
 		inputer.showInputer();
 		inputer.showNextQst();
 		inputer.listen();
